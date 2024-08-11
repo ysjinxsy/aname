c4937545667eaf3d5e208086b2a21260f546d7ff
ysjinxsy <168466901+ysjinxsy@users.noreply.github.com>
2024-08-11 18:36:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import nextcord #type: ignore
 from nextcord.ext import commands #type: ignore
 import aiosqlite #type: ignore
 import logging
+import os
 from shared import guild_id
 
 logging.basicConfig(level=logging.INFO)
@@ -12,9 +13,9 @@ guild_id = [1266153230300090450, 1221092843288920065]
 client = commands.Bot(command_prefix="?", intents=intents)
 DATABASE_PATH = "database.db"
 
-
+tokeng = os.environ.get('token')
 # Assuming you have commands defined in commands.py
 from commands import *
 
-client.run(process.env.token)
+client.run(tokeng)
 
