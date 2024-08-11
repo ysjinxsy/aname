a66df48961e56a09e75a47fd27e88bfb35cf5975
ysjinxsy <168466901+ysjinxsy@users.noreply.github.com>
2024-08-11 20:05:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import aiosqlite #type: ignore
 import logging
 import os
 from shared import guild_id
-tokeng = "MTI1NjAwMjgwMTg2ODIxMDMxNg.GQqCD4.Zl7oLgcT7Yw8a_Tt78jGkVXgjcj8BaNZzRA9g4"
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -13,7 +12,7 @@ guild_id = [1266153230300090450, 1221092843288920065]
 client = commands.Bot(command_prefix="?", intents=intents)
 DATABASE_PATH = "database.db"
 
-tokeng = os.environ.get('token')
+tokeng = os.getenv('token')
 # Assuming you have commands defined in commands.py
 from commands import *
 
