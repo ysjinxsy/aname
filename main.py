7b5793fa75295a2af9bfd5ae3eeaae6a7733aa48
ysjinxsy <168466901+ysjinxsy@users.noreply.github.com>
2024-08-11 18:24:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import nextcord #type: ignore
+from nextcord.ext import commands #type: ignore
+import aiosqlite #type: ignore
+import logging
+from shared import guild_id
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+intents = nextcord.Intents.all()
+guild_id = [1266153230300090450, 1221092843288920065]
+client = commands.Bot(command_prefix="?", intents=intents)
+DATABASE_PATH = "database.db"
+
+
+# Assuming you have commands defined in commands.py
+from commands import *
+
+client.run("MTI1NjAwMjgwMTg2ODIxMDMxNg.GRQGky.MROrlqow58OPGFf8vq-F-YaoUmuNH5IglDazUk")
+
