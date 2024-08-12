3969e87229b9d93e4c293285802458ffa57a35f4
ysjinxsy <168466901+ysjinxsy@users.noreply.github.com>
2024-08-12 22:25:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 intents = nextcord.Intents.all()
-guild_id = 1221092843288920065
+guild_id = 1259483488227168256
 client = commands.Bot(command_prefix="?", intents=intents)
 DATABASE_PATH = "database.db"
 
